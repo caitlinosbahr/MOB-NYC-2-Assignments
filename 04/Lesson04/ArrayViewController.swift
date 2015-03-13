44f714064133007a6f623600aec5ef70b81acca5
theletterc <cosbahr@gmail.com>
2015-03-13 14:38:47 -0400

    Button adds text to table

--- 04/Lesson04/ArrayViewController.swift
+++ 04/Lesson04/ArrayViewController.swift
@@ -1,14 +1,6 @@
-//
-//  ViewController.swift
-//  Lesson04
-//
-//  Created by Rudd Taylor on 9/28/14.
-//  Copyright (c) 2014 General Assembly. All rights reserved.
-//
-
 import UIKit
 
-class ArrayViewController: UIViewController {
+class ArrayViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,8 +8,43 @@ class ArrayViewController: UIViewController {
         /*
         TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
         TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
-        
         */
+        
     }
+
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func tappedAddButton(sender: AnyObject) {
+        addCell()
+    }
+    
+    var rows = [String]()
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows.count
+    }
+
+    func addCell() {
+        rows.append(textField.text)
+        tableView.reloadData()
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as UITableViewCell
+        cell.textLabel!.text = rows[rows.count - 1 - indexPath.row]
+        return cell
+    }
+    
+//    func textFieldShouldReturn (textField: UITextField) -> Bool {
+//        addCell()
+//        textField.resignFirstResponder()
+//        return true
+//    }
+
+    
 }
 
