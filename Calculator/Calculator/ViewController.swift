d2e234c34a166a47532b72a045e5e2dd6acf5cc1
theletterc <cosbahr@gmail.com>
2015-03-28 11:26:51 -0400

    Initial commit
    Initial layout and basic connections

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Caitlin Osbahr on 3/26/15.
+//  Copyright (c) 2015 Caitlin Osbahr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var calcLabel: UILabel!
+
+
+    @IBAction func equals(sender: AnyObject) {
+    }
+
+
+    @IBAction func clear(sender: AnyObject) {
+    }
+    
+    @IBAction func multiply(sender: AnyObject) {
+    }
+    
+    @IBAction func subtract(sender: AnyObject) {
+    }
+    
+    @IBAction func add(sender: AnyObject) {
+    }
+    
+    @IBAction func divide(sender: AnyObject) {
+    }
+    
+    @IBAction func numberTapped(sender: AnyObject) {
+    }
+    
+    
+}
+
