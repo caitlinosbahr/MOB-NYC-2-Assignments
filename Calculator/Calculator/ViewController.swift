fa04e66c68936a6cd6b078fe7cabb2c266866952
theletterc <cosbahr@gmail.com>
2015-03-28 12:31:32 -0400

    Added the function logic, but things are not quite right

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number = Int()
+    var operation = ""
+    var result = Int()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,29 +22,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var calcLabel: UILabel!
     
     
+    //How do you append another number? Right now this just replaces the current number
+    @IBAction func numberTapped(sender: UIButton) {
+        number = sender.titleLabel!.text!.toInt()!
+        
+        calcLabel.text = "\(number)"
+    }
+    
+    
+    @IBAction func operationTapped(sender: UIButton) {
+        operation = sender.titleLabel!.text! as String //Should I add a label that shows the current operation somewhere to make sure this is doing the right thing?
+    }
+    
 
+    //For some reason this is borked right now
     @IBAction func equals(sender: AnyObject) {
+        if operation == "+" {
+            result = result + number
+        } else if operation == "-" {
+            result = result - number
+        } else if operation == "x" {
+            result = result * number
+        } else if operation == "/" {
+            result = result / number
+        } else {
+            result = number //Not sure this is really what you want here?
+        }
+        
+        calcLabel.text = "\(result)"
     }
 
 
     @IBAction func clear(sender: AnyObject) {
+        number = 0
+        result = 0
+        operation = "="
+        calcLabel.text = "\(result)"
     }
     
-    @IBAction func multiply(sender: AnyObject) {
-    }
     
-    @IBAction func subtract(sender: AnyObject) {
-    }
+
     
-    @IBAction func add(sender: AnyObject) {
-    }
-    
-    @IBAction func divide(sender: AnyObject) {
-    }
-    
-    @IBAction func numberTapped(sender: UIButton) {
-        calcLabel.text = sender.titleLabel!.text!
-    }
+
     
     
 }
